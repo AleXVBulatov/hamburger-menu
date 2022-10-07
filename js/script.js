f183921e2b14a4a917118ca6c52dcd19aa44bc30
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2022-10-07 13:12:13 +0300

    Разработан первый вариант работы

--- js/script.js
+++ js/script.js
@@ -1,58 +1,58 @@
 window.addEventListener("DOMContentLoaded", () => {
 
 
-  const nameRef = document.querySelector(".name");
-  // console.log(nameRef);
-  const eventTypeRef = document.querySelector(".js_event-type");
-  // console.log(eventTypeRef);
-  const eventCodeRef = document.querySelector(".js_event-code");
-  // console.log(eventCodeRef);
-  const eventKeyCodeRef = document.querySelector(".js_event-keyCode");
-  // console.log(eventKeyCodeRef);
-  const eventKeyRef = document.querySelector(".js_event-key");
-  // console.log(eventKeyRef);
-  const sectionRef = document.querySelectorAll(".section");
-  // console.log(sectionRef);
-  const activeModalRef = document.querySelector(".js_active-modal");
-  console.log(activeModalRef);
-  const modalButton = document.querySelector(".modal__button");
-  console.log(modalButton);
+  // const nameRef = document.querySelector(".name");
+  // // console.log(nameRef);
+  // const eventTypeRef = document.querySelector(".js_event-type");
+  // // console.log(eventTypeRef);
+  // const eventCodeRef = document.querySelector(".js_event-code");
+  // // console.log(eventCodeRef);
+  // const eventKeyCodeRef = document.querySelector(".js_event-keyCode");
+  // // console.log(eventKeyCodeRef);
+  // const eventKeyRef = document.querySelector(".js_event-key");
+  // // console.log(eventKeyRef);
+  // const sectionRef = document.querySelectorAll(".section");
+  // // console.log(sectionRef);
+  // const activeModalRef = document.querySelector(".js_active-modal");
+  // console.log(activeModalRef);
+  // const modalButton = document.querySelector(".modal__button");
+  // console.log(modalButton);
 
 
 
-  modalButton.addEventListener("mouseover", (event) => {
-    event.target.style.cursor = "pointer";    
-  });
+  // modalButton.addEventListener("mouseover", (event) => {
+  //   event.target.style.cursor = "pointer";    
+  // });
 
-  modalButton.addEventListener("click", (event) => {
-    // console.log(event.target);    
-    if (event.target) {
-      activeModalRef.style.display = "none";
-    };
-  });
+  // modalButton.addEventListener("click", (event) => {
+  //   // console.log(event.target);    
+  //   if (event.target) {
+  //     activeModalRef.style.display = "none";
+  //   };
+  // });
   
 
-  window.addEventListener("keydown", (event) => {
-    nameRef.innerText = event.keyCode;
-    eventTypeRef.innerText = event.type;
-    eventCodeRef.innerText = event.code;
-    eventKeyCodeRef.innerText = event.keyCode;
-    eventKeyRef.innerText = event.key;
-  });
+  // window.addEventListener("keydown", (event) => {
+  //   nameRef.innerText = event.keyCode;
+  //   eventTypeRef.innerText = event.type;
+  //   eventCodeRef.innerText = event.code;
+  //   eventKeyCodeRef.innerText = event.keyCode;
+  //   eventKeyRef.innerText = event.key;
+  // });
 
 
-  sectionRef.forEach((elem) => {
-    // console.log(elem);
-    elem.addEventListener("mouseover", (event) => {
-      event.currentTarget.style.cursor = "pointer";
-      event.currentTarget.classList.toggle("active");
-      event.currentTarget.style.transition = "500ms";
-    });
+  // sectionRef.forEach((elem) => {
+  //   // console.log(elem);
+  //   elem.addEventListener("mouseover", (event) => {
+  //     event.currentTarget.style.cursor = "pointer";
+  //     event.currentTarget.classList.toggle("active");
+  //     event.currentTarget.style.transition = "500ms";
+  //   });
 
-    elem.addEventListener("mouseout", (event) => {
-      event.currentTarget.classList.toggle("active");
-    });
-  });
+  //   elem.addEventListener("mouseout", (event) => {
+  //     event.currentTarget.classList.toggle("active");
+  //   });
+  // });
 
 });
 
