dbd3243a59d732483c57f2cb47d3e19d8836a5d9
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2022-10-07 15:40:01 +0300

    Дополняет JS код

--- js/script.js
+++ js/script.js
@@ -1,58 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-
-  // const nameRef = document.querySelector(".name");
-  // // console.log(nameRef);
-  // const eventTypeRef = document.querySelector(".js_event-type");
-  // // console.log(eventTypeRef);
-  // const eventCodeRef = document.querySelector(".js_event-code");
-  // // console.log(eventCodeRef);
-  // const eventKeyCodeRef = document.querySelector(".js_event-keyCode");
-  // // console.log(eventKeyCodeRef);
-  // const eventKeyRef = document.querySelector(".js_event-key");
-  // // console.log(eventKeyRef);
-  // const sectionRef = document.querySelectorAll(".section");
-  // // console.log(sectionRef);
-  // const activeModalRef = document.querySelector(".js_active-modal");
-  // console.log(activeModalRef);
-  // const modalButton = document.querySelector(".modal__button");
-  // console.log(modalButton);
+  const menuBtnRef = document.querySelector(".menu__btn");
+  // console.log(menuBtnRef);
+  const menuBoxRef = document.querySelector(".menu__box");
+  // console.log(menuBoxRef);
 
 
-
-  // modalButton.addEventListener("mouseover", (event) => {
-  //   event.target.style.cursor = "pointer";    
-  // });
-
-  // modalButton.addEventListener("click", (event) => {
-  //   // console.log(event.target);    
-  //   if (event.target) {
-  //     activeModalRef.style.display = "none";
-  //   };
-  // });
-  
-
-  // window.addEventListener("keydown", (event) => {
-  //   nameRef.innerText = event.keyCode;
-  //   eventTypeRef.innerText = event.type;
-  //   eventCodeRef.innerText = event.code;
-  //   eventKeyCodeRef.innerText = event.keyCode;
-  //   eventKeyRef.innerText = event.key;
-  // });
-
-
-  // sectionRef.forEach((elem) => {
-  //   // console.log(elem);
-  //   elem.addEventListener("mouseover", (event) => {
-  //     event.currentTarget.style.cursor = "pointer";
-  //     event.currentTarget.classList.toggle("active");
-  //     event.currentTarget.style.transition = "500ms";
-  //   });
-
-  //   elem.addEventListener("mouseout", (event) => {
-  //     event.currentTarget.classList.toggle("active");
-  //   });
-  // });
-
+  menuBtnRef.addEventListener("click", (event) => {
+    if (event.currentTarget.nodeName === "DIV") {
+      menuBoxRef.classList.toggle("js_active");
+      menuBtnRef.classList.toggle("js_btn-close");
+    }
+    window.addEventListener("click", (event) => {
+      if (event.target.nodeName === "BODY" && menuBtnRef.classList.contains("js_btn-close")) {
+        menuBoxRef.classList.toggle("js_active");
+        menuBtnRef.classList.toggle("js_btn-close");
+      };
+      return;
+    });
+  });
 });
 
